3f61fede474c458ba1ccba6b177ebf25e5c0aa3c
Anveks <vekselmana@gmail.com>
2023-10-21 11:01:24 +0300

    .

--- script.js
+++ script.js
@@ -5,17 +5,13 @@ const cube = document.querySelector(".cube");
 const p1 = document.querySelector(".player-1");
 const p2 = document.querySelector(".player-2");
 
-const p1_score = p1.querySelector('.dice-score');
-const p2_score = p2.querySelector('.dice-score');
-
-const p1_current = p1.querySelector('.score');
-const p2_current = p2.querySelector('.score');
-
 const roll_button = document.querySelector('.roll-btn');
 const settings_button = document.querySelector('.settings-btn');
 const restart_button = document.querySelector('.restart-btn');
 const hold_button = document.querySelector('.hold-btn');
 
+let gameOver = false;
+let endGameMessage;
 let current_score = 0;
 let activePlayer = 1;
 
@@ -62,32 +58,7 @@ const rollDice = () => {
   if (randomDice > 1) {
     current_score += randomDice;
 
-    activePlayer === 1 
-      ? p1_current.innerHTML = current_score 
-      : p2_current.innerHTML = current_score
-
-    hold_button.addEventListener('click', () => {
-      console.log('the current player is: ' + activePlayer);
-
-      const activePlayerElement = document.querySelector(`.player-${activePlayer}`);
-
-      // Update the current player's score
-      const playerScoreElement = activePlayerElement.querySelector('.dice-score');
-      const playerCurrentElement = activePlayerElement.querySelector('.score');
-    
-      playerScoreElement.innerHTML = Number(playerScoreElement.innerHTML) + current_score;
-      playerCurrentElement.innerHTML = 0;
-      current_score = 0;
-    
-      // Toggle the "active" class to indicate the change
-      p1.classList.toggle("active");
-      p2.classList.toggle("active");
-    
-      // Switch to the other player
-      activePlayer === 1 
-        ? activePlayer = 2 
-        : activePlayer = 1;
-    });
+    document.querySelector(`.player-${activePlayer}`).querySelector('.score').innerHTML = current_score;
     
   } else {
     // Toggle the "active" class for both players
@@ -103,5 +74,33 @@ const rollDice = () => {
       : activePlayer = 1;
   };
 };
+
+hold_button.addEventListener('click', () => {
+  console.log('the current player is: ' + activePlayer);
+
+  const activePlayerElement = document.querySelector(`.player-${activePlayer}`);
+
+  // Update the current player's score
+  const playerScoreElement = activePlayerElement.querySelector('.dice-score');
+  const playerCurrentElement = activePlayerElement.querySelector('.score');
+
+  playerScoreElement.innerHTML = Number(playerScoreElement.innerHTML) + current_score;
+  playerCurrentElement.innerHTML = 0;
+  current_score = 0;
+
+  if (playerScoreElement.innerHTML >= 100) {
+    gameOver = true;
+    endGameMessage = `Congratulations! Player #${activePlayer} has won! 🎉`;
+  }
+
+  // Toggle the "active" class to indicate the change
+  p1.classList.toggle("active");
+  p2.classList.toggle("active");
+
+  // Switch to the other player
+  activePlayer === 1 
+    ? activePlayer = 2 
+    : activePlayer = 1;
+});
 
 
